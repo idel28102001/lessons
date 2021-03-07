6fc1a539018fb0b580ee5450aafce5a07c111daf
Stivans <radik_1995@Mail.ru>
2021-03-07 19:26:58 +0300

    Second

---
+++ light_2.py
@@ -0,0 +1,24 @@
+# Задание номер 1
+for i in range(5):
+    print(i,0)
+# Задание номер 2
+l = 0
+for i in range(10):
+    num = int(input('Введите цифру: '))
+    if num==5:
+        l+=1
+print(l)
+# Задание номер 3
+l = 0
+for i in range(100):
+    l+=i
+print(l)
+# Задание номер 4
+l = 1
+for i in range(1,10):
+    l*=i
+print(l)
+# Задание номер 5
+num = input('Введите число: ')
+for i in num:
+    print(i)
