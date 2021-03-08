f88f5034d8c275175dacf66ba5d0342622c1aa50
Stivans <radik_1995@Mail.ru>
2021-03-08 13:44:33 +0300

    Second

--- Lesson_2/up.py
+++ Lesson_2/up.py
@@ -2,20 +2,16 @@ print('Загадайте число')
 num = 'да'
 l = 4
 while num == 'да':
-    l-=1
-    num = input(f'Количество цифр вашего числа меньше {l}? : ')## да или нет
+    l -= 1
+    num = input(f'Количество цифр вашего числа меньше {l}? : ')  ## да или нет
 num_2 = 'да'
 dig = ''
-while l>0:
+while l > 0:
     number = 10
-    while num_2=='да':
-        number-=1
-        num_2 = input(f'Ваша {l}-e цифра меньше {number}? :')# да или нет
-    l-=1
-    dig=str(number) + dig
-    num_2='да'
+    while num_2 == 'да':
+        number -= 1
+        num_2 = input(f'Ваша {l}-e цифра меньше {number}? :')  # да или нет
+    l -= 1
+    dig = str(number) + dig
+    num_2 = 'да'
 print(dig)
-
-
-
-
